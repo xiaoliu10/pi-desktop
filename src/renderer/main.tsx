1567d2a5ed0b51582ec25f87692f636a9b0a7c8e
jason <xiaoliu10@users.noreply.github.com>
2026-09-24 15:18:20 +0800

    style: 代码查看器语法高亮与 diff 行颜色对齐 ZCode（GitHub 主题 + @pierre/diffs 混合比）

--- src/renderer/main.tsx
+++ src/renderer/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
-import 'highlight.js/styles/github-dark.css';
 
 /**
  * Two explicit entries:
